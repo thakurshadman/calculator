636442d33c25fc052e7cebfd6947f0575272c9ad
shadman <thakurshadman@outlook.com>
2022-03-29 20:26:03 -0400

    Calculator UI ONLY grid practice

---
+++ script.js
@@ -0,0 +1,19 @@
+let current = 0;
+let a = 0;
+let b = 0; 
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
